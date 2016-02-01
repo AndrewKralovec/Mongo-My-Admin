edbbd18a96de311b60c7ea500a0f5ca92843c0a7
AndrewKralovec <akrala@siu.edu>
2016-01-31 20:08:24 -0600

    Array handling

--- public/javascripts/adminCtrl.js
+++ public/javascripts/adminCtrl.js
@@ -37,8 +37,8 @@ app.controller('customersCtrl', function ($scope, $http) {
 
     // Call refresh to init cantacklist 
     refresh();
-    $scope.runTest = function(){
-        alert(10);}; 
+    $scope.isArray = angular.isArray;
+        
     $scope.editDB = function (DB) {
         $scope.menu.collection = true ; 
         $scope.menu.database = false ; 
